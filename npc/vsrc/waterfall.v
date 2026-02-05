a296fd2863413e1c37bce20cf8d4e22c370e89c4
Joiry-Hi <joiry-hi@outlook.com>
2026-02-05 11:15:26 +0800

    Fixed some thing

--- npc/vsrc/waterfall.v
+++ npc/vsrc/waterfall.v
@@ -1,17 +1,25 @@
 module waterfall(
   input clk,
   input rst,
-  output [15:0] ledr
+  output [15:0] led
 );
   reg [31:0] count;
-  reg [15:0] led;
+  reg [15:0] ledr;
+  // wire a, b; // For testing warning function
+
   always @(posedge clk) begin
-    if (rst) begin led <= 16'b1101001000100001; count <= 0; end
+    if (rst) begin ledr <= 16'b1101001000100001; count <= 0; end
     else begin
-      if (count == 0) led <= {led[14:0], led[15]};
+      if (count == 0) ledr <= {ledr[14:0], ledr[15]};
       count <= (count >= 5000000 ? 32'b0 : count + 1);
     end
   end
 
-    assign ledr = led;
+  // always_comb begin
+  //   a = b;
+  //   b = 1;
+  // end
+
+  assign led = ledr;
+
 endmodule
